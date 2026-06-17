e738b9fa28f9437cb81ce7934d157f7e9698b1b6
RayanXmonitoring <evaanjelinaaswitalumbanggaol@gmail.com>
2026-06-17 12:20:38 +0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'device-management-backend',
+      script: 'server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+      },
+      env_development: {
+        NODE_ENV: 'development',
+        PORT: 3001,
+      },
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
+      log_file: './logs/pm2-combined.log',
+      time: true,
+    },
+  ],
+};
